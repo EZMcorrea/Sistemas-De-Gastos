d8c536e478fee4a831799f9a2962b2bcf1995b83
Everton Correa <evertonzcorrea@gmail.com>
2026-04-14 09:12:09 -0300

    Test: expect home redirect to /gastos

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Home redirects to the expenses page
+        $response->assertRedirect('/gastos');
     }
 }
